c95398ad848641af185f9ad797ba6118af35a862
nobeeakon <nobeeakon@gmail.com>
2021-02-16 12:39:53 -0700

    Modified filter pokemon tests added filterPokemon tests. Also added validator for the filteringPokemon

--- src/utils/filterPokemon.js
+++ src/utils/filterPokemon.js
@@ -1,4 +1,10 @@
 export default function filterPokemon(pokeList, name) {
+  // validate pokeList
+  if (!Array.isArray(pokeList)) return [];
+
+  // validate name
+  if (typeof name !== "string") return [];
+
   if (name === "") return [];
   return (
     Array.isArray(pokeList) &&
--- src/utils/filterPokemon.test.js
+++ src/utils/filterPokemon.test.js
@@ -3,28 +3,56 @@ import { mockPokemonsData } from "../mock/pokeData";
 
 describe("Filter Pokemon", () => {
   test("it should return an array", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
+    let name = "a";
+    let result = filterPokemon(mockPokemonsData, name) instanceof Array;
+    expect(result).toBe(true);
   });
 
   test("it should return an empty array", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
+    let name = "";
+    let result = filterPokemon(mockPokemonsData, name).length;
+    expect(result).toBe(0);
   });
 
   test("it should return an array with charizard object", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
+    let name = "charizard";
+    const charizard = [
+      {
+        name: "charizard",
+        sprites: {
+          front_default:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+          front_shiny:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png"
+        },
+        videoLink: "https://www.youtube.com/watch?v=evvO5Gcl-mo"
+      }
+    ];
+    let result = filterPokemon(mockPokemonsData, name);
+    expect(result).toStrictEqual(charizard);
   });
 
   /* BUNUS */
   test("it should validate the 'pokeList' argument", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
+    let resultNull = filterPokemon(null, "a");
+    let resultUndefined = filterPokemon(undefined, "a");
+    let resultNotArray = filterPokemon("", "a");
+
+    expect(resultNull).toStrictEqual([]);
+    expect(resultUndefined).toStrictEqual([]);
+    expect(resultNotArray).toStrictEqual([]);
   });
 
   test("it should validate the 'name' argument", () => {
-    /* Your solution comes here */
-    expect(false).toBe(true);
+    let resultNull = filterPokemon(mockPokemonsData, null);
+    let resultUndefined = filterPokemon(mockPokemonsData, undefined);
+    let resultNotArray = filterPokemon(mockPokemonsData, [
+      "charizard",
+      "zangoose"
+    ]);
+
+    expect(resultNull).toStrictEqual([]);
+    expect(resultUndefined).toStrictEqual([]);
+    expect(resultNotArray).toStrictEqual([]);
   });
 });
